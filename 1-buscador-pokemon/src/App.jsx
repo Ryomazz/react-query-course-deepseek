68352955b64b158394939db655e426cec4ebb124
Ryomaz <space.devb@gmail.com>
2025-05-17 07:11:43 -0400

    Arreglado el bug cuando no encuentra le pokemon

--- 1-buscador-pokemon/src/App.jsx
+++ 1-buscador-pokemon/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+   QueryClient,
+   QueryClientProvider,
+   useQuery,
+} from "@tanstack/react-query";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient();
 
-  return (
-    <>
+const fetchPokemon = async (pokemonName) => {
+   try {
+      const response = await fetch(
+         `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+      );
+      if (!response.ok) throw new Error("Pokemon no encontrado");
+      return response.json();
+   } catch (error) {
+      throw new Error(error.message);
+   }
+};
+
+function Pokemon({ pokemonName }) {
+   const { isPending, error, data } = useQuery({
+      queryKey: ["pokemon", pokemonName],
+      queryFn: () => fetchPokemon(pokemonName),
+   });
+
+   if (isPending) return <span>Loading...</span>;
+
+   if (error) {
+      return <span>Error: {error.message}</span>;
+   }
+
+   return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <h1>{data.name}</h1>
+         <img src={data.sprites?.front_default} alt="" />
+         {data.stats &&
+            data.stats.map((stat) => {
+               const { base_stat } = stat;
+               const stat_name = stat.stat.name;
+
+               return (
+                  <h2 key={stat_name}>
+                     {stat_name}--{base_stat}{" "}
+                  </h2>
+               );
+            })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   );
 }
 
-export default App
+function App() {
+   const [query, setQuery] = useState("");
+   const [pokemonName, setPokemonName] = useState("");
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      setPokemonName(query);
+   };
+
+   return (
+      <section>
+         <h1>Pokemon TanStack Query</h1>
+         <form>
+            <input
+               type="text"
+               placeholder="Charizard, Pickachu..."
+               value={query}
+               onChange={(e) => setQuery(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Search</button>
+         </form>
+         <QueryClientProvider client={queryClient}>
+            {pokemonName && <Pokemon pokemonName={pokemonName}></Pokemon>}
+         </QueryClientProvider>
+      </section>
+   );
+}
+export default App;
